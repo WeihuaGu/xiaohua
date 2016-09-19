8f91a676c28e67e4db6395336114a47dd02367cd
weihuagu <ministarmail@msn.cn>
2016-09-19 11:25:26 +0800

    oo

--- templates/nav.php
+++ templates/nav.php
@@ -1,6 +1,7 @@
+ <div class="navbar-fixed">
 <nav>      
 				<div class="nav-wrapper">      	
-					<a href="/" class="brand-logo">&nbsp &nbsp趣图|笑话</a>
+					<a href="/" class="hide-on-small-only brand-logo">&nbsp &nbsp趣图|笑话</a>
 					<ul id="nav-mobile" class="right">			
 						<li>
 							<a href="/xiaohua">笑话</a>
@@ -15,3 +16,4 @@
 					</ul>      
 				</div>
 </nav>
+</div>
